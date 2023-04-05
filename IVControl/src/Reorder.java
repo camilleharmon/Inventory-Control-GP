9b0bb73e8b7f8d6bc754769fbb89758655032b01
brie.brocato <brie.brocato@MU-C1-18.mullen.pvt.k12.co.us>
2023-04-05 13:42:41 -0600

    Reworked writing to file and reorder class.

--- IVControl/src/Reorder.java
+++ IVControl/src/Reorder.java
@@ -1,25 +1,30 @@
 public class Reorder
 	{
 		public static void main(String[] args) {
-	      reorder(); 
+		reorder();	
 
 	    }	
 	
 	public static void reorder()
 		{
-		String fileName = "Tracking.txt";
-		int storeAccount = 10000;
+		 
+		int storeAccount = 100000;
 		for(int i = 0; i < Customer.items.size(); i++)
 			{
-			if(Customer.items.get(i).getAmount() < 10000)
+			
+			if(Customer.items.get(i).getAmount() < 5)
 				{
 				double wholesale = Customer.items.get(i).getWholesaleCost() * 15;
 				System.out.println(wholesale);
 				if(storeAccount < wholesale)
 					{
 					Customer.items.get(i).setAmount(Customer.items.get(i).getAmount() + 15);
+					WritingToFile.reorderWrite(i);
 					
-					WritingToFile.reorderWrite(fileName);
+					}
+				else
+					{
+					WritingToFile.noMoney(i);
 					}
 				}
 			}
--- IVControl/src/WritingToFile.java
+++ IVControl/src/WritingToFile.java
@@ -6,11 +6,11 @@ public class WritingToFile
     	{
         // The name of the file to open.
         String fileName = "Tracking.txt";
-        
     	}
     
-    public static void reorderWrite(String fileName)
+    public static void reorderWrite(int i)
     	{
+    	String fileName = "Tracking.txt";
     	try {
             // Assume default encoding.
             FileWriter fileWriter = new FileWriter(fileName, true);
@@ -20,11 +20,38 @@ public class WritingToFile
 
             // Note that write() does not automatically
             // append a newline character.
-            bufferedWriter.write("Hello there,");
-            bufferedWriter.write(" here is some text.");
+            bufferedWriter.write(Customer.items.get(i).getName() + "has been reordered.  Amount was increase by 15.");
             bufferedWriter.newLine();
-            bufferedWriter.write("We are writing");
-            bufferedWriter.write(" this text to the file.");
+            
+
+            // Always close files.
+            bufferedWriter.close();
+            System.out.println("Test");
+        	}
+        
+        catch(IOException ex) 
+        	{
+            System.out.println("Error writing to file '" + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        	}
+    	}
+    
+    public static void noMoney(int i)
+    	{
+    	String fileName = "Tracking.txt";
+    	try {
+            // Assume default encoding.
+            FileWriter fileWriter = new FileWriter(fileName, true);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write("There was not enough money to reorder " + Customer.items.get(i).getName());
+            bufferedWriter.newLine();
+            
 
             // Always close files.
             bufferedWriter.close();
